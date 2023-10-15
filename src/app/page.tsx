9e911ab8bdc8a03243270a366d638faf403f4ebe
Asier Marqués <asiermarques@gmail.com>
2023-10-15 19:40:37 +0200

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,13 +18,15 @@ export default function Home() {
                             <Link href={"https://linkedin.com/in/asier"} className="btn btn-lg btn-outline-secondary">Find me on Linkedin</Link>
                         </div>
                     </section>
-                    <section className="follow-me row justify-content-md-center">
-                        <p className={"col col-sm-10"}>You can also find me on <br className={"d-block d-sm-none"}/>
-                            {findMyOn.map((platform, index) =>
-                            <><Link title={platform.name} key={platform.link} href={platform.link}>{platform.name}</Link>{findMyOn.length > 1 && index === findMyOn.length-2 ? ", and" : ","} </>) }
-                            or book <Link title={"book a virtual meeting"} href={"https://calendly.com/asiermarques"}>a virtual coffee</Link>&nbsp;☕ in my calendar.</p>
-                        <div className="separation">
-                            * * *
+                    <section className="follow-me">
+                        <div className="row justify-content-md-center">
+                            <p className={"col col-sm-10"}>You can also find me on <br className={"d-block d-sm-none"}/>
+                                {findMyOn.map((platform, index) =>
+                                <><Link title={platform.name} key={platform.link} href={platform.link}>{platform.name}</Link>{findMyOn.length > 1 && index === findMyOn.length-2 ? ", and" : ","} </>) }
+                                or book <Link title={"book a virtual meeting"} href={"https://calendly.com/asiermarques"}>a virtual coffee</Link>&nbsp;☕ in my calendar.</p>
+                            <div className="separation">
+                                * * *
+                            </div>
                         </div>
                     </section>
                     <section className={"writings"}>
