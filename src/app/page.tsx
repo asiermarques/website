5e4d3edeced089f1fb290648b66660bcb1286a65
Asier Marqués <asiermarques@gmail.com>
2023-10-15 15:38:13 +0200

    text and style improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
             <section className="professional">
                     <section className="introduction">
                         <div>
-                            <p>I&apos;m a results-oriented professional <strong>building and leading high-performance software engineering teams</strong>, including international, distributed, remote, and outsourced members.</p>
+                            <p>I&apos;m a results-oriented professional <strong>building and leading high-performance software engineering teams</strong><span className={"d-none d-sm-inline"}>, including international, distributed, remote, and outsourced members</span>.</p>
                             <p>My ultimate goal is to contribute to people&apos;s growth by <strong>coaching and mentoring engineers, leaders, and managers</strong>.</p>
                             <Link href={"https://linkedin.com/in/asier"} className="btn btn-lg btn-outline-secondary">Find me on Linkedin</Link>
                         </div>
