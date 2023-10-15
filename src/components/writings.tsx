dc144630ba1fdc3d62ce87cea27098cd44e584d5
Asier Marqués <asiermarques@gmail.com>
2023-10-15 21:33:37 +0200

    refactor in components

--- src/components/writings.tsx
+++ src/components/writings.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import {articles} from "@/data";
 
 export default function Writings () {
-    return                     <section className={"writings"}>
+    return <section className={"writings"}>
         <h3>📝 Writings</h3>
         {articles.map(article => <article key={article.link}>
             <Link title={article.title} href={article.link}>
