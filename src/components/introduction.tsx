11fd605be734c1c3e1bd1495447d96077b1be20b
Asier Marqués <asiermarques@gmail.com>
2023-10-19 18:45:34 +0200

    text

--- src/components/introduction.tsx
+++ src/components/introduction.tsx
@@ -4,8 +4,7 @@ import Link from "next/link";
 export default function Introduction () {
     return <section className="introduction">
         <div>
-            <p>I&apos;m a results-oriented professional
-                <strong>building and leading high-performance software engineering teams</strong>
+            <p>I&apos;m a results-oriented professional <strong>building and leading high-performance software engineering teams</strong>
                 <span className={"d-none d-sm-inline"}>, including international, distributed, remote, and outsourced members</span>.
             </p>
             <p>My ultimate goal is to contribute to people&apos;s growth by <strong>coaching and mentoring engineers, leaders, and managers</strong>.</p>
